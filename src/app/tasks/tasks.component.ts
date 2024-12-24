5de9f6ffa2195d1f9831c8a0edb5d9360a88704c
ashreposit <anithaasha12@gmail.com>
2024-12-24 10:07:17 +0530

    output specific task

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, Output,EventEmitter } from '@angular/core';
+import { TaskComponent } from './task/task.component';
+
+@Component({
+  selector: 'app-tasks',
+  imports: [TaskComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css'
+})
+export class TasksComponent {
+  
+  @Input({required:true}) userId!:string;
+  @Input({required:true}) name!:string;
+
+  tasks=[
+    {
+      id:'t1',
+      userId:'u1',
+      title:'Master Angular',
+      summary:'Learn all the basic and advanced features of Angular',
+      dudeDate:'2025-12-31'
+    },
+    {
+      id:'t2',
+      userId:'u2',
+      title:'Master Angular',
+      summary:'Learn all the basic and advanced features of Angular',
+      dudeDate:'2025-12-31'
+    }
+  ]
+  
+  get selectedUserId(){
+    return this.tasks.filter((task)=> task.userId === this.userId);
+  }
+}
