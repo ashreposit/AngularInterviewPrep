64abdc5cf6c56770e9bc10a1e55f984e2df6afa9
ashreposit <anithaasha12@gmail.com>
2024-12-24 09:54:38 +0530

    type aliases & interfaces,ouputting conditional content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,25 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent,UserComponent],
+  imports: [HeaderComponent,UserComponent,TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  users=DUMMY_USERS;
 
-  onSelectUser(id:string){
-    console.log("Selected user id is " + id);
+  users = DUMMY_USERS;
+  selectedUserId?:string;
+
+  get onSelectUser(){
+    return this.users.find((user)=>user.id === this.selectedUserId)!;
+  }
+
+  onSelect(id:string){
+    this.selectedUserId = id;
   }
 }
---
+++ src/app/task/task.component.ts
@@ -0,0 +1,13 @@
+import { Component,Input,Output,EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  imports: [],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent {
+
+  @Input({required:true}) name!:string;
+
+}
