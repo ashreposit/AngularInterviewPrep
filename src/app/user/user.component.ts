46425193f75b37ca44f8b7baf26db77827d09786
ashreposit <anithaasha12@gmail.com>
2024-12-24 09:49:00 +0530

    output decorator and emitting input data using event emitters

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input,input } from '@angular/core';
+import { Component,Input,Output,EventEmitter,output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,16 +8,18 @@ import { Component, Input,input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent{
-  // @Input({required:true}) avatar!:string;
-  // @Input({required:true}) name!:string;
-  avatar = input.required<string>();
-  name = input.required<string>();
+
+  @Input({required:true}) id!:string;
+  @Input({required:true}) avatar!:string;
+  @Input({required:true}) name!:string;
+
+  @Output() select = new EventEmitter<string>();
 
   get imagePath(){
-    return 'assets/users/'+ this.avatar();
+    return 'assets/users/'+ this.avatar;
   }
 
   onSelectUser(){
-
+    this.select.emit(this.id);
   }
 }
