35e1e977dff06c0a820de38fe9141d4fe3ed48d8
ashreposit <anithaasha12@gmail.com>
2024-12-24 15:40:09 +0530

    created new modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { BrowserModule } from "@angular/platform-browser";
-import { CardComponent } from "./shared/card/card.component";
-import { DatePipe } from "@angular/common";
-import { FormsModule } from "@angular/forms";
-import { TaskComponent } from "./tasks/task/task.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+import { SharedModule } from "./shared/shared.module";
+import { TasksModule } from "./tasks/tasks.module";
 
 @NgModule({
-    declarations:[AppComponent,HeaderComponent,UserComponent,CardComponent,TasksComponent,TaskComponent,NewTaskComponent],
-    bootstrap:[AppComponent],
-    imports:[BrowserModule,DatePipe,FormsModule]
+    declarations: [AppComponent, HeaderComponent, UserComponent],
+    bootstrap: [AppComponent],
+    imports: [BrowserModule,SharedModule,TasksModule]
 })
-export class AppModule{};
+export class AppModule { };
